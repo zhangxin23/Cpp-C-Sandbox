1fa921ce986b6e8f41315bd93d45862bc34c02f2
zhangxin5 <zhangxin5@kingsoft.com>
2015-11-04 14:33:56 +0800

    clion

--- dfs/combination.cpp
+++ dfs/combination.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 void comb(vector<vector<int> >&v, vector<int> &mv, int n, int k, int cur) {
     if(mv.size() == k) { //终止条件或者收敛条件
         v.push_back(mv);
--- dfs/restore_ip_addresses.cpp
+++ dfs/restore_ip_addresses.cpp
@@ -1,17 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isValidIpItem(string item) {
+	return true;
+}
+
 void dfs(string origTxt, vector<string> result, string item, int segIndex, int start) {
 	//终止条件
 	if(start >= origTxt.length())
 		return;
 
 	if(segIndex == 4) {
-		string subStr = origTxt.subStr(start);
+		string subStr = origTxt.substr(start);
 		if(isValidIpItem(subStr))
 			result.push_back(item + "." + subStr);
 		return;
 	}
 
 	for(int i = 1; i < 4 && ((start + i) <= origTxt.length()); i++) {
-		string subStr = origTxt.subStr(start, start + i);
+		string subStr = origTxt.substr(start, start + i);
 		if(isValidIpItem(subStr)) {
 			if(segIndex == 1) {
 				dfs(origTxt, result, subStr, segIndex + 1, start + i);
