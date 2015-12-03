e0491ec5a2507f455cb52938d83af527aae8d01b
zhangxin5 <zhangxin5@kingsoft.com>
2015-12-03 10:21:05 +0800

    vector<vector<int>> vs vector<vector<int> >

--- container/vector.cpp
+++ container/vector.cpp
@@ -39,4 +39,14 @@ int main() {
 
     vec.clear();
     cout << "after clear, size: " << vec.size() << endl;
+
+    cout << endl;
+
+    vector<vector<int>> table(10, vector<int>(10, 1));
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            cout << table[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
